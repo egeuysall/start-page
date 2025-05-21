b7acae4b0612599a2e675145962ac53f262aa8cd
Ege Uysal <hello@egeuysal.com>
2025-05-21 09:38:33 -0500

    Update browser.tsx

--- frontend/src/components/ui/browser.tsx
+++ frontend/src/components/ui/browser.tsx
@@ -18,8 +18,9 @@ export const Browser: React.FC = () => {
         value={input}
         onChange={(e) => setInput(e.target.value)}
         placeholder="Search Google"
+        className="py-2 px-3 rounded-lg placeholder:opacity-50 placeholder:text-primary-200 focus:ring-primary-200 text-primary-200 transition duration-200 text-base md:text-lg tracking-tight bg-accent-100"
       />
-      <button type="submit">Search</button>
+      <button type="submit" className="px-3 py-2 rounded-lg bg-accent-100 text-primary-200 transition duration-200 text-base md:text-lg tracking-tight font-bold hover:opacity-50">Search</button>
     </form>
   );
 };
