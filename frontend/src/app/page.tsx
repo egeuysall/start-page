7e91db86b2c2fcd99ece2a1508dc358ab49c7181
Ege Uysal <hello@egeuysal.com>
2025-05-21 00:16:01 -0500

    Update page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Clock } from "@/components/ui/clock";
 import { Welcome } from "@/components/ui/welcome";
-import { Link } from "@/components/ui/Link";
+import { Link } from "@/components/ui/link";
 import { Quotes } from "@/components/ui/quotes";
 
 // Import the link data
