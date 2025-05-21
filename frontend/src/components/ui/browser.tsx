6da70993c4ebc0b5541b93d58224b951a8bb86a0
Ege Uysal <hello@egeuysal.com>
2025-05-21 09:47:15 -0500

    Update browser.tsx

--- frontend/src/components/ui/browser.tsx
+++ frontend/src/components/ui/browser.tsx
@@ -12,13 +12,13 @@ export const Browser: React.FC = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className="w-full grid gap-4 grid-cols-1 md:grid-cols-4">
       <input
         type="text"
         value={input}
         onChange={(e) => setInput(e.target.value)}
         placeholder="Search Google"
-        className="py-2 px-3 rounded-lg placeholder:opacity-50 placeholder:text-primary-200 focus:ring-primary-200 text-primary-200 transition duration-200 text-base md:text-lg tracking-tight bg-accent-100"
+        className="md:col-span-3 py-2 px-3 rounded-lg placeholder:opacity-50 placeholder:text-primary-200 focus:ring-primary-200 text-primary-200 transition duration-200 text-base md:text-lg tracking-tight bg-accent-100"
       />
       <button type="submit" className="px-3 py-2 rounded-lg bg-accent-100 text-primary-200 transition duration-200 text-base md:text-lg tracking-tight font-bold hover:opacity-50">Search</button>
     </form>
